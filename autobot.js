fcf0c983dec7172dbfb52037c8ae21527e70089e
JJlamA <alexp228@icloud.com>
2016-11-15 06:59:53 -0800

    Add files via upload

---
+++ autobot.js
@@ -0,0 +1,52 @@
+var Discord = require("discord.js");
+var bot = new Discord.Client();
+
+bot.on("guildMemberAdd", (member) => {
+    var guild = member.guild;
+    guild.channels.get(guild.id).sendMessage(member.user.username + " is waiting in #doormat!")
+});
+
+
+bot.on("guildMemberUpdate", (oldMember, newMember) => {
+    if(oldMember.roles.size != newMember.roles.size) {
+    	var guild = oldMember.guild;
+        let newRoles = newMember.roles.has(guild.roles.find(role => role.name == "Accepted Terms" )) && !oldMember.roles.has(bot.roles.find( role => role.name == "Role name here" )) // gets the new role(s)
+        console.log("Please welcome " + oldMember.user.username + " to the server!");
+    }
+});
+
+// bot.on("guildMemberRemove", (guild, member) => {
+//     guild.channels.get(guild.id).sendMessage("Goodbye," + member + "... You will be missed!")
+// });
+
+bot.on("message", msg => {
+    var guild = msg.channel.guild;
+    if (guild) {
+	    var member = guild.member(msg.author);
+	    if (msg.channel.id == (guild.channels.find(channel => channel.name == "doormat")).id) {
+	    	if (msg.content.toLowerCase().startsWith("accepted rules")) {
+		        if (member.roles.exists("name", "Accepted Terms")) {
+		        	msg.delete()
+		            member.sendMessage("You already accepted the terms and conditions!")
+		            return;
+		        }
+		        else {
+		        	member.addRole(guild.roles.find(role => role.name == "Accepted Terms" ).id);
+		        	bot.channels.get(guild.channels.find(channel => channel.name == "general" ).id).sendMessage(member + " has just accepted the terms and conditions!");
+		        	member.sendMessage("Thank you for accepting the terms and conditions! Be sure to follow the rules and be respectful!")
+		        	msg.delete()
+		        }
+		    }
+		    else {
+		    	msg.delete()
+		    }	
+	    }
+	}    
+});
+
+bot.on('ready', () => {
+  console.log('AutobotGD ready for action!');
+  bot.user.setGame("Made by JJlamA");
+});
+
+bot.login("MjQ3Mzc3NTkxODYwODU0Nzg0.CwoT2w.E4V8znDmYJx9SSC9TF7FgdN0hW4");
